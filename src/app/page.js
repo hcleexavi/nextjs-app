efce1f89be8ea1c65e1c43d1a1dddbb627599e75
Xavier 207 <xaverius@nate.com>
2023-08-31 17:37:41 +0900

    제외 하고 커밋

--- src/app/page.js
+++ src/app/page.js
@@ -4,17 +4,8 @@ export default function Home() {
   return (
     <>
     <h2>Welcome</h2>
-    안녕하세요. WEB!
     
+    안녕하세요. WEB! 반갑소
     </>
   )
 }
-// 깃 테스트
-
-// 깃 테스트 2
-
-// 깃 테스트 3
-
-// 깃 테스트 4
-
-// 깃 테스트 5
